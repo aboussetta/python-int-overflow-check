c0e529553c30ff61e0b16f5e6006082e93c1e13c
Elmer Medez <emedez@palominodb.com>
2013-11-28 02:11:32 +0000

    Imported Upstream version 0.1b.1.dev

--- ci/build-ubuntu-12.04-x86_64.sh
+++ ci/build-ubuntu-12.04-x86_64.sh
@@ -2,7 +2,17 @@
 set -x
 set -e
 sudo apt-get install python-setuptools
+cd $WORKSPACE
+
+# clean up existing old files
+rm -rf *.log
+rm -rf *.build
+rm -rf *.changes
+rm -rf *.tar.gz
+rm -rf *.dsc
+rm -rf *.deb
 rm -rf $WORKSPACE/result
+
 mkdir -p $WORKSPACE/result
 cd $WORKSPACE/debian-debian
 git-buildpackage --git-builder="DIST=precise ARCH=amd64 pdebuild --buildresult ../result/" 2>&1 | tee ../build.log
